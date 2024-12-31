54885f55f5fc1d522f0823f9d786da01ab5ca0f7
Paupog <154589264+Paupog@users.noreply.github.com>
2024-12-31 14:55:26 +0100

    Checked if working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather app
+   <h1>Weather app</h1> 
+    <footer>
+      This project was coded by Paulina Pogwizd and open-sourced on {""}
+     <a href="https://github.com/Paupog/react-weatha" target="_blank" rel="noreferrer">
+     Github </a>
+    </footer>
     </div>
   );
 }
