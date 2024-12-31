7c86c8931e890b65dbb272ce0787aa4a733f797e
Paupog <154589264+Paupog@users.noreply.github.com>
2024-12-31 15:09:27 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
    <h1>Weather app</h1> 
     <footer>
       This project was coded by Paulina Pogwizd and open-sourced on {""}
      <a href="https://github.com/Paupog/react-weatha" target="_blank" rel="noreferrer">
      Github </a>
     </footer>
+    </div>
     </div>
   );
 }
