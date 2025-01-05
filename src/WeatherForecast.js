6e7014c13cb2c557af40e4cf68e59d9fba7f718b
Paupog <154589264+Paupog@users.noreply.github.com>
2025-01-05 19:21:48 +0100

    Provided Api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast(){
+export default function WeatherForecast(props){
+    function handleResponse (response) {
+        console.log(response.data); 
+    }
+
+
+    let apiKey= "dfd0t6478f0367aa31fdf9o1c7ab1790";
+    let apiUrl= `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
+    
+    axios.get(apiUrl).then(handleResponse);
+    
     return (
         <div className="WeatherForecast">
             <div className="row">
