677c9c18c3c386a546420dc7bdd3aba4fd2608f2
Paupog <154589264+Paupog@users.noreply.github.com>
2025-01-05 18:27:39 +0100

    Displayed one day

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex align-items-center">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={54} />
             <div>
               <WeatherTemperature celsius={props.data.temperature} />
              
